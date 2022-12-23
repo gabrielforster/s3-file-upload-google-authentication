229441e9f25f0fe4a37e0e6cf0d2f75dc72bcbbb
gabrielforster <rochafrgabrie@gmail.com>
2022-12-23 00:00:06 -0300

    tailwindcss setup

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-zinc-800 text-white'>
+      <h2>Hello from frontend</h2>
+    </div>
+  )
+}
